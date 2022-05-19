9301792a28c3e687200b2f48a019d556047e5a30
LucasLiu20200131 <60515906+LucasLiu20200131@users.noreply.github.com>
2022-05-19 11:38:13 +0800

    Update rank_vis.R

--- R/rank_vis.R
+++ R/rank_vis.R
@@ -1,4 +1,4 @@
-rank_vis <- function(SRGA.result){
+rank_vis <- function(SRGA.result,top.show=5){
   for(pkg in c('scales','ggplot2')){
     if(!requireNamespace(pkg,quietly=T)){
       stop(paste("The ",pkg," package needed for this function to work. Please install it.",sep=""),
@@ -29,5 +29,11 @@ rank_vis <- function(SRGA.result){
     theme_classic()+
     xlab('Number of genes')+
     ylab('Rank')
+  if(top.show){
+    gene.show = paste(rownames(all_rank_score)[1:top.show],collapse = '\n')
+    p1=p1+
+      geom_segment(aes(x = number[1], y = rank[1], xend = nrow(all_rank_score)/10, yend = 0.3),colour = "black",lty = 1)+
+      geom_label(x=nrow(all_rank_score)/10,y=0.3,label=gene.show,label.size=0.5)
+  }
   return(p1)
 }
