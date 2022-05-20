42df54158a36fe35f9620ff4ec9bc5f90323b61d
LucasLiu20200131 <60515906+LucasLiu20200131@users.noreply.github.com>
2022-05-20 20:24:02 +0800

    Update correlation_calculation.R

--- R/correlation_calculation.R
+++ R/correlation_calculation.R
@@ -1,5 +1,5 @@
-correlation_calculation <- function(mRNA_exp,covariate=NULL,select.name){
-  for(pkg in c('Hmisc')){
+fgsea_calculation <-function (RS,signatures,scale.flag=FALSE){
+  for(pkg in c('fgsea','scales')){
     if(!requireNamespace(pkg,quietly=T)){
       stop(paste("The ",pkg," package needed for this function to work. Please install it.",sep=""),
            call. = FALSE)
@@ -7,36 +7,32 @@ correlation_calculation <- function(mRNA_exp,covariate=NULL,select.name){
       library(pkg,warn.conflicts=F,character.only = T)
     }
   }
-  
-  # PCC
-  fun_mtx_pcr <- function(x,y,z){
-    r12=cor(t(x),t(y))
-    r13=cor(t(x),z)
-    r23=cor(z,t(y))
-    r123=r13%*%r23
-    rup=r12-r123
-    rd1=sqrt(1-r13*r13)
-    rd2=sqrt(1-r23*r23)
-    rd=rd1%*%rd2
-    rrr=rup/rd
-    return(rrr)
+  fgsea_all <- c()
+  for(i in 1:nrow(RS)){
+    ##remove self-correlation
+    mRNA <- rownames(RS)[i]
+    ranks <- RS[i,-grep(mRNA,colnames(RS))]
+    ##remove Infs
+    ranks <- ranks[!is.infinite(ranks)]
+    ranks <- sapply(ranks, as.numeric)
+    fgseaRes <- fgsea(signatures, ranks, minSize=1, maxSize=5000, nperm=10000)
+    addname = names(signatures)[!names(signatures) %in% fgseaRes$pathway]
+    if(length(addname)){
+    addrow = data.frame(pathway=addname,
+                        pval=NA,
+                        padj=NA,
+                        ES=NA,
+                        NES=NA,
+                        nMoreExtreme=NA,
+                        size=NA,
+                        leadingEdge=NA)
+    fgseaRes = rbind(fgseaRes,addrow)}
+    fgseaRes$sigValue <- -log10(fgseaRes$pval) * fgseaRes$NES
+    fgseaRes <- cbind(mRNA,fgseaRes)
+    fgsea_all <- rbind(fgsea_all,fgseaRes)
   }
-  
-  if(is.null(covariate)){
-    # cor
-    cor.result=Hmisc::rcorr(t(mRNA_exp))
-    pcor = cor.result$r
-    p.value = cor.result$P
-  }else{
-    # partial cor
-    n=length(mRNA_exp)
-    gn=1
-    pcor <- fun_mtx_pcr(mRNA_exp,mRNA_exp,covariate)
-    statistic <- pcor*sqrt((n-2-gn)/(1-pcor^2))
-    p.value <- 2*pnorm(-abs(statistic))
+  if(scale.flag){
+    fgsea_all$sigValue <- scales::rescale(fgsea_all$sigValue,c(-1,1))
   }
-  RS <- -log10(p.value)*sign(pcor)
-  rownames(RS) <- rownames(mRNA_exp)
-  colnames(RS) <- rownames(mRNA_exp)
-  return(RS = RS[select.name,])
+  return(fgsea_all)
 }
